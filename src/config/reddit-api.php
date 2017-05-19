3aa00ab8f1bfc105b65460b7e1c78f6d88362b78
Gino Messmer <g.messmer@ftsoftware.eu>
2017-05-19 18:36:09 +0200

    Support for environment config settings

--- src/config/reddit-api.php
+++ src/config/reddit-api.php
@@ -4,12 +4,22 @@ return [
     'endpoint_standard' => 'https://www.reddit.com',
     'endpoint_oauth' => 'https://oauth.reddit.com',
     
-    'username' => '',
-    'password' => '',
-    'app_id' => '',
-    'app_secret' => '',
+    'username' => env('REDDIT_USERNAME', ''),
+    'password' => env('REDDIT_PASSWORD', ''),
+
+    'app_id' => env('REDDIT_API_ID', ''),
+    'app_secret' => env('REDDIT_API_SECRET', ''),
     
     'response_format' => 'STD', // STD | ARRAY
     
     'scopes' => 'save,modposts,identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,submit,subscribe,vote,wikiedit,wikiread'
 ];
+
+//
+// .env
+//
+// REDDIT_USERNAME=
+// REDDIT_PASSWORD=
+// REDDIT_API_ID=
+// REDDIT_API_SECRET=
+//
